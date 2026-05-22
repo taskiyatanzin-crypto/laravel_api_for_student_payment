546f65adca1edc5de51a75896f0cbec61a98af07
shahed-64 <shahedislam6475@gmail.com>
2026-05-22 12:31:41 +0600

    fix root route for api backend

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Laravel API is running'
+    ]);
+});
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
